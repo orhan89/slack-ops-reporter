e624b8369b7e5f050dccdafe76d09931dd2be252
Ricky Hariady <ricky@sense-os.nl>
2024-07-23 18:57:25 +0700

    serve both socket mode and flask

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-import os
 from slack_ops_reporter import app
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from flask import Flask, request
 
-SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+import os
+
+handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
+handler.connect()
+
+flask_app = Flask(__name__)
+
+
+@flask_app.route("/", methods=["GET"])
+def index():
+    return "Hello World"
